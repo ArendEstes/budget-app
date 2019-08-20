4d3ec529a4f15571fa4880cee0c5a613160122ca
arendEstes <arendestes@yahoo.com>
2019-08-20 09:42:10 -0500

    display budget when submitted in budgegt input

--- js/app.js
+++ js/app.js
@@ -15,4 +15,55 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  //submit budget method
+  submitBudgetForm(){
+    let value = this.budgetInput.value;
+
+    if(value === "" || value < 0){
+      this.budgetFeedback.classList.add("showItem");
+      this.budgetFeedback.innerHTML = `Positive value must be entered`;
+      setTimeout(()=>{
+        this.budgetFeedback.classList.remove("showItem");
+      }, 4000)
+    }
+    else{
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = "";
+    }
+    
+  }
 }
+
+
+
+function eventListeners(){
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+  // new instance of UI CLASS
+  const ui = new UI();
+
+  //budget form submit
+  budgetForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    ui.submitBudgetForm();
+  })
+
+   //expense form submit
+   expenseForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    
+  })
+
+   //expense click
+   expenseList.addEventListener("click", function(){
+    
+  })
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  eventListeners();
+})
+
